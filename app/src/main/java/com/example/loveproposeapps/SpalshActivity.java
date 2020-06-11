2c443d7dc1894cdbb01fbb48c9d49bf62e18e8b7
Ramim <ramimhridoy512@gmail.com>
2020-06-11 19:05:34 +0600

    first commit

---
+++ app/src/main/java/com/example/loveproposeapps/SpalshActivity.java
@@ -0,0 +1,33 @@
+package com.example.loveproposeapps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class SpalshActivity extends AppCompatActivity {
+
+
+    private ImageView imageView;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spalsh);
+
+        imageView= findViewById(R.id.imageView22ID);
+        button= findViewById(R.id.Letsgo22ID);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(SpalshActivity.this,MainActivity.class);
+                startActivity(in);
+            }
+        });
+    }
+}
